0d6148ce524876c4932d67a20174b1a53416b5cf
PenPeen <ikegami1022.h@gmail.com>
2026-01-17 22:24:47 +0900

    feat: DynamoDBテーブルの定義

---
+++ infra/constructs/database.py
@@ -0,0 +1,25 @@
+from aws_cdk import (
+    RemovalPolicy,
+    aws_dynamodb as dynamodb,
+)
+from constructs import Construct
+
+class Database(Construct):
+    """DynamoDB Construct"""
+
+    def __init__(self, scope: Construct, id: str) -> None:
+        super().__init__(scope, id)
+
+        self.table = dynamodb.Table(
+            self, "TripTable",
+            partition_key = dynamodb.Attribute(
+                name = "PK",
+                type = dynamodb.AttributeType.STRING
+            ),
+            sort_key = dynamodb.Attribute(
+                name = "SK",
+                type = dynamodb.AttributeType.STRING
+            ),
+            billing_mode = dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy = RemovalPolicy.DESTROY
+        )
--- serverless_trip_saga_stack.py
+++ serverless_trip_saga_stack.py
@@ -2,8 +2,11 @@ from aws_cdk import (
     Stack,
 )
 from constructs import Construct
+from infra.constructs import Database   # infra/constructs/__init__.py を load
 
 class ServerlessTripSagaStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        database = Database(self, "Database")
