da8787f5227082850783eeee2f26393bb86ddc25
Pen Peen <ikegami1022.h@gmail.com>
2026-01-17 14:48:45 +0900

    改行確保

--- tests/unit/test_serverless_trip_saga_stack.py
+++ tests/unit/test_serverless_trip_saga_stack.py
@@ -7,6 +7,6 @@ def test_stack_created():
     app = core.App()
     stack = ServerlessTripSagaStack(app, "ServerlessTripSagaStack")
     template = assertions.Template.from_stack(stack)
-    
+
     # Verify the stack is empty as currently defined
     template.resource_count_is("AWS::SQS::Queue", 0)
