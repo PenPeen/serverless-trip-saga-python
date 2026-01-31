71893299791ebc3094626df7318b36466c17de0c
PenPeen <ikegami1022.h@gmail.com>
2026-01-31 23:12:25 +0900

    Lintエラーが発生していた。

--- services/hotel/domain/factory/hotel_booking_factory.py
+++ services/hotel/domain/factory/hotel_booking_factory.py
@@ -3,6 +3,7 @@ from typing import TypedDict
 
 from services.hotel.domain.entity.hotel_booking import HotelBooking
 from services.hotel.domain.enum.hotel_booking_status import HotelBookingStatus
+from services.hotel.domain.value_object.hotel_booking_id import HotelBookingId
 from services.hotel.domain.value_object.hotel_name import HotelName
 from services.hotel.domain.value_object.stay_period import StayPeriod
 from services.shared.domain.value_object.currency import Currency
@@ -23,18 +24,20 @@ class HotelDetails(TypedDict):
 class HotelBookingFactory:
     """ホテル予約情報を生成するFactory"""
 
-    def create(self, trip_id: TripId, hotel_details: HotelDetails) -> HotelBooking
+    def create(self, trip_id: TripId, hotel_details: HotelDetails) -> HotelBooking:
         """新規予約のエンティティを作成する"""
 
-        booking_id = HotelBooking.from_trip_id(trip_id)
+        booking_id = HotelBookingId.from_trip_id(trip_id)
 
         hotel_name = HotelName(hotel_details["hotel_name"])
         stay_period = StayPeriod(
             check_in=hotel_details["check_in_date"],
             check_out=hotel_details["check_out_date"],
         )
-        price = Money(amount=hotel_details["price_amount"])
-        currency = Currency(hotel_details["price_currency"])
+        price = Money(
+            amount=hotel_details["price_amount"],
+            currency=Currency(hotel_details["price_currency"]),
+        )
 
         return HotelBooking(
             id=booking_id,
